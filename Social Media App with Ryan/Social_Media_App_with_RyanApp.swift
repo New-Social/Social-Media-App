aea1ac5aafbe7f6425d9a0a3d16d1088e7088d57
Brayton-Lordianto <braylord30@gmail.com>
2023-03-19 17:53:10 -0400

    Initial Commit

---
+++ Social Media App with Ryan/Social_Media_App_with_RyanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Media_App_with_RyanApp.swift
+//  Social Media App with Ryan
+//
+//  Created by Brayton Lordianto on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Social_Media_App_with_RyanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
